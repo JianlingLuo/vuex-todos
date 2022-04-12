61a332e57c0dc61a5a388c5a94bfb338bd87d171
luojianling <luojianling>
2022-04-12 11:21:17 +0800

    upload code

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <a-input placeholder="请输入任务" v-model="newItem" class="my_ipt" />
+    <a-button type="primary" @click="addNew">添加事项</a-button>
+
+    <a-list bordered :dataSource="alisList" class="dt_list">
+      <a-list-item slot="renderItem" slot-scope="item">
+        <!-- 复选框 -->
+        <a-checkbox :checked='item.done' @change="onChange(item)">{{ item.info }}</a-checkbox>
+        <!-- 删除链接 -->
+        <a slot="actions" @click="deleteOne(item.id)">删除</a>
+      </a-list-item>
+
+      <!-- footer区域 -->
+      <div class="footer" slot="footer">
+        <span>共{{alisList.length}}条剩余</span>
+        <a-button-group>
+          <a-button :type="curType == 'all' ? 'primary': 'default'" @click="changeCurType('all')">全部</a-button>
+          <a-button :type="curType == 'undo' ? 'primary': 'default'" @click="changeCurType('undo')">未完成</a-button>
+          <a-button :type="curType == 'done' ? 'primary': 'default'" @click="changeCurType('done')">已完成</a-button>
+        </a-button-group>
+        <a @click="cleanDone()">清除已完成</a>
+      </div>
+    </a-list>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import {mapGetters, mapMutations, mapState} from 'vuex'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "app",
+  data() {
+    return {
+      newItem: ''
+      // list: [],
+    };
+  },
+  computed: {
+    ...mapState(['list', 'curType']),
+    ...mapGetters(['alisList'])
+  },
+  created() {
+    this.$store.dispatch('getList');
+  },
+  methods: {
+    ...mapMutations(['changeCurType', 'cleanDone', 'deleteOne']),
+    addNew() {
+      let con = this.newItem.trim();
+      if (con.length == 0) {
+        this.$message.warn('请先输入内容');
+        return;
+      }
+      this.$store.commit('addNew', con);
+      this.newItem = ''
+    },
+    onChange(item) {
+      this.$store.commit('updateItemStatus', item.id);
+    }
   }
-}
+};
 </script>
-
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  padding: 10px;
+}
+.my_ipt {
+  width: 500px;
+  margin-right: 10px;
+}
+.dt_list {
+  width: 500px;
+  margin-top: 10px;
+}
+.footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,63 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    list: [],
+    curType: 'all',
+    curId: 6
   },
   mutations: {
+    setList(state, data){
+      state.list = data;
+    },
+    changeCurType(state, type) {
+      if (!type) return;
+      state.curType = type;
+    },
+    cleanDone(state) {
+      state.list = state.list.filter(one => one.done == false);
+    },
+    deleteOne(state, id) {
+      let index = state.list.findIndex(one => one.id == id);
+      console.log(index)
+      state.list.splice(index,1);
+    },
+    addNew(state, todo) {
+      let one = {
+        id:state.curId,
+        info: todo,
+        done: false
+      }
+      state.list.push(one);
+      state.curId++;
+    },
+    updateItemStatus(state, id) {
+      let item = state.list.find(one => one.id == id);
+      item.done = !item.done;
+    }
   },
   actions: {
+    getList(context) {
+      axios.get('./data.json').then(res => {
+        console.log(res.data);
+        context.commit('setList', res.data);
+      })
+    }
+  },
+  getters: {
+    alisList(state) {
+      if(state.curType == 'undo') {
+        return state.list.filter(one => one.done == false);
+      }
+      if (state.curType == 'done') {
+        return state.list.filter(one => one.done == true);
+      }
+      return state.list;
+    }
   },
   modules: {
   }
